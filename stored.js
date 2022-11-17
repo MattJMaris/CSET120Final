e7adf69aed506ff9b9739e49fc22f1fe19f1f99c
MattJMaris <mjmaris221@stevenscollege.edu>
2022-11-17 08:03:28 -0500

    Added login page and signup pages with the basic storage script

---
+++ stored.js
@@ -0,0 +1,30 @@
+function passwordHide() {
+    var x = document.getElementById("placeholder");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password"
+    }
+}
+
+var inputKey = document.getElementById("inputKey");
+var inputValue = document.getElementById("inputValue")
+var btnInsert = document.getElementById("btnInsert");
+var lsOutput = document.getElementById("lsOutput")
+
+btnInsert.onclick = function() {
+    var key = inputKey.ariaValueMax;
+    var value = inputValue.value;
+
+    if (key && value) {
+        localStorage.setItem(key, value);
+        location.reload();
+    }
+}
+
+for (let i=0; i<localStorage.length; i++) {
+    var key = localStorage.key(i);
+    var value = localStorage.getItem(key);
+
+    lsOutput.innerHTML += `${key}`;
+}
