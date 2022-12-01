d08494ff1c72c51ac6fbcbc94a1a2de19fc277bd
coal <deadcoal21@gmail.com>
2022-12-01 09:30:38 -0500

    push

--- Manager.js
+++ Manager.js
@@ -5,23 +5,24 @@ function ManagerLog(){
         e.preventDefault();
     let username ="Manager1"
     let password = "Manager1"
-        inputUser = document.querySelector('inputKey')
-        inputPass = document.querySelector('inputValue')
-        if (inputUser === "Eli" || inputPass === "password") {
+        inputUser = document.getElementById('inputKey').value
+        inputPass = document.getElementById('inputValue').value
+        if (inputUser === "Eli" && inputPass === "password") {
             alert("Username and Password do not match. <br>Please try again.")
+            // location.href = "http://127.0.0.1:5500/CSET120Final/manager-landing.html";
         }
-        if (inputUser === username && inputPass === password) {
+        else if(inputUser === username && inputPass === password) {
             alert("Success! <br>You are now signin")
+            location.href = "http://127.0.0.1:5500/CSET120Final/manager-landing.html";
         }
-        btnInsert.onclick = function() {
-            var key = inputKey.value;
-            var value = inputValue.value;
-        
-            if (key && value) {
-                localStorage.setItem(key, value);
-                location.href = "http://127.0.0.1:5500/CSET120Final/manager-landing.html"; 
+        else { alert("wrong info loser")
+            
+        }
+            if (inputUser && inputPass) {
+                localStorage.setItem(inputUser, inputPass);
+                // location.href = "http://127.0.0.1:5500/CSET120Final/manager-landing.html"; 
             }
-        };
-    })
-}
+        });
+    }
+    
     window.onload = ManagerLog
