38a1ffb65cb583d07cbbe74ea834361bd47a9c4b
MattJMaris <mjmaris221@stevenscollege.edu>
2022-12-12 11:11:04 -0500

    Finished Design For button (mostly) and refixed JS on my-end

--- Fooditems.js
+++ Fooditems.js
@@ -3,132 +3,132 @@ var foodItems = [
         "id":1,
         "image": "img/Pizza Napoletana.png",
         "name": "Pizza Napoletana",
-        "price": "12.99",
+        "price": "$12.99",
 
     },
     {
         "id":2,
         "image": "img/Polenta.png",
         "name": "Polenta",
-        "price": "14.99",
+        "price": "$14.99",
     },
     {
         "id":3,
         "image": "img/Ribollita.png",
         "name": "Ribollita",
-        "price": "10.99",
+        "price": "$10.99",
 
     },
     {
         "id":4,
         "image": "img/Risotto alla Milanese.png",
         "name": "Risotto alla Milanese",
-        "price": "8.49",
+        "price": "$8.49",
     },
     {
         "id":5,
         "image": "img/Cotoletta alla milanese.png",
         "name": "Cotoletta alla milanese",
-        "price": "14.49",
+        "price": "$14.49",
     },
     {
         "id":6,
         "image": "img/Osso Buco alla Milanese.png",
         "name": "Osso Buco alla Milanese",
-        "price": "11.99",
+        "price": "$11.99",
     },
     {
         "id":7,
         "image": "img/bkmnfffz.bmp",
         "name": "Lasange",
-        "price": "9.49",
+        "price": "$9.49",
     },
     {
         "id": 8,
         "image": "img/Bottarga.png",
         "name": "Bottarga",
-        "price": "4.99",
+        "price": "$4.99",
     },
     {
         "id":9,
         "image": "img/Baccalà.png",
         "name": "Baccalà",
-        "price": "5.99",
+        "price": "$5.99",
     },
     {
         "id":10,
         "image": "img/Vitello Tonnato.png",
         "name": "Vitello Tonnato",
-        "price": "12.49",
+        "price": "$12.49",
     },
     {
         "id":11,
         "image": "img/Fritto Misto.png",
         "name": "Fritto Misto",
-        "price": "16.99",
+        "price": "$16.99",
 
     },
     {
         "id":12,
         "image": "img/Ragù alla Bolognese.png",
         "name": "Ragù all Bolognese",
-        "price": "15.99",
+        "price": "$15.99",
 
     },
     {
         "id":13,
         "image": "img/Bistecca Fiorentina.png",
         "name": "Bistecca Fiorentina",
-        "price": "23.99",
+        "price": "$23.99",
 
     },
     {
         "id":14,
         "image": "img/Arancini.png",
         "name": "Arancini",
-        "price": "7.49",
+        "price": "$7.49",
 
     },
     {
         "id":15,
         "image": "img/Melanzane alla Parmigiana.png",
         "name": "Melanzane alla Parmigiana",
-        "price": "18.49",
+        "price": "$18.49",
 
     },
     {
         "id":16,
         "image": "img/Saltimbocca alla Romana.png",
         "name": "Saltimbocca alla Romana",
-        "price": "13.99",
+        "price": "$13.99",
 
     },
     {
         "id":17,
         "image": "img/Cacio e Pepe.png",
         "name": "Cacio e Pepe",
-        "price": "12.49",
+        "price": "$12.49",
 
     },
     {
         "id":18,
         "image": "img/Gnocchi.png",
         "name": "Gnocchi",
-        "price": "6.99",
+        "price": "$6.99",
 
     },
     {
         "id":19,
         "image": "img/Spaghetti alle Vongole.png",
         "name": "Spaghetti alle Vongole",
-        "price": "13.99",
+        "price": "$13.99",
 
     },
     {
         "id":20,
         "image": "img/strawberry.jpg",
-        "name": "Strawberry Cheesecak",
-        "price": "19.99",
+        "name": "Strawberry Cheesecake",
+        "price": "$19.99",
 
     }
 ];
--- ManagerLanding.js
+++ ManagerLanding.js
@@ -8,11 +8,11 @@ function renderManagerMenu() {
             <div class="menulist">
                 <div class="categories">
                     <div class="shop-item">
-                        <ion-icon class="removeButton" onclick="removeFromMenu(${foodItem.id})" name="close-circle-outline"></ion-icon>
+                        <ion-icon class="removeButton" onclick="removeFromMenu(${foodItem.id})" name="close-circle-outline">Remove</ion-icon>
                         <img class="shop-item-image" src="${foodItem.image}" alt="">
                         <h3 class="itemName">${foodItem.name}</h3>
                         <div class="shop-item-details">
-                        <span class="shop-item-price">$9.99</span> <br> 
+                        <span class="shop-item-price">${foodItem.price}</span> <br> 
                         <button class="btn btn-primary shop-item-button" type="button">Item back to cart</button>
                         </div>
                     </div>
@@ -26,14 +26,12 @@ function removeFromMenu(id) {
     localStorage.setItem("foodItems", JSON.stringify(foodItems));
     renderManagerMenu();
 };
-const addNewMenuItemBtn = document.getElementById('addNewMenuItem');
+const addNewMenuItemBtn = document.getElementById('addNewMenuItemBtn');
 addNewMenuItemBtn.addEventListener('click', () => {
-    const name = document.getElementById('newItemName').value;
-    const price = document.getElementById('newItemPrice').value;
-    const description = document.getElementById('newItemDescription').value;
-    const image = document.getElementById('newItemImg').value;
-    const calories = document.getElementById('newItemCalories').value;
-    const id = document.getElementById('newItemId').value;
+    const id = document.getElementById('id').value;
+    const image = document.getElementById('image').value;
+    const name = document.getElementById('name').value;
+    const price = document.getElementById('prices').value;
     const id1 = parseInt(id)
     for(let i = 0; i < foodItems.length; i++) {
         if(foodItems[i].name === name) {
@@ -61,7 +59,7 @@ const appendMenuItems = () => {
     let newMenuItem1 = document.createElement('div')  
     newMenuItem1.classList.add('menuItem')
     newMenuItem1.innerHTML =  `
-    <ion-icon class="removeButton" onclick="removeFromMenu(${foodItems.id})" name="close-circle-outline"></ion-icon>
+    <ion-icon class="removeButton" onclick="removeFromMenu(${foodItems.id})" name="close-circle-outline">Remove</ion-icon>
     <img class="shop-item-image" src="${foodItems.image}" alt="">
     <h3 class="itemName">${foodItems.name}</h3>
     <div class="shop-item-details">
